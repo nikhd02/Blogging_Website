f78fd3477bbd7b0e8884fb1dccad8b9770d30397
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-09-12 03:09:30 +0530

    .

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
 
 
 import User from './Schema/User.js';
@@ -20,10 +22,27 @@ mongoose.connect(process.env.DB_LOCATION, {
     autoIndex: true
 });
 
-// const generateUsername = async (email) => {
-//     let username = email.split("@")[0];
+const formateDatatoSend = () => {
 
-// }
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY)
+
+    return {
+    access_token,
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname
+    }
+}
+
+const generateUsername = async (email) => {
+    let username = email.split("@")[0];
+
+    let isUsernameNotUnique = await User.exists({ "personal_info": username }).then((result) => result)
+    isUsernameNotUnique ? username += nanoid().substring(0, 5) : "";
+
+    return username;
+
+}
 
 server.post("/signup",(req, res) => {
     // res.json(req.body)
@@ -49,16 +68,16 @@ server.post("/signup",(req, res) => {
     }
         // HASHING THE PASSWORD
 
-    bcrypt.hash(password, 10, (err, hashed_password) => {
+    bcrypt.hash(password, 10,async (err, hashed_password) => {
 
-        let username = email.split("@")[0];
+        let username = await generateUsername(email);
         
         let user = new User({
             personal_info:{ fullname, email, password: hashed_password, username }
         })
 
         user.save().then((u) => {
-            return res.status(200).json({ user: u })
+            return res.status(200).json(formateDatatoSend(u))
         })
         .catch(err => {
             // return res.status(500).json({ "error": err.massage })
@@ -73,6 +92,20 @@ server.post("/signup",(req, res) => {
 
     // return res.status(200).json({"status": "Okay"})
 
+})
+
+server.post("/signin", (req, res ) => {
+    let { email, password } = req.body;
+
+    user.findOne({ "personal_info.email": email })
+    .then((user) => {
+        console.log(user)
+        return res.json({ "status": "got user document" })
+    })
+    .catch((err) => {
+        console.log(err);
+        return res.status(500).json({"error": "User not found"})
+    })
 })
 
 server.listen(PORT, () => {
