ba506abe40592d590261d2625234d2f29313a2ec
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-09-15 16:04:25 +0530

    Update navbar.component.jsx

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -93,6 +93,7 @@ import { Link, Outlet } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import { useContext, useState } from "react";
 import { UserContext } from "../App";
+import UserNavigationPanel from "./user-navigation.component";
 
 const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
@@ -134,9 +135,18 @@ const Navbar = () => {
                         <>
                             <Link to="/dashboard/notification">
                                 <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
-                                    {/* Notification Icon or Profile Image */}
+                                <i className="fi fi-rs-bell text-2xl block mt-1"></i>
                                 </button>
                             </Link>
+                            <div className="relative">
+
+                                <button className="w-12 h-12 mt-1">
+                                    <img src={profile_img} className="w-full h-full object-cover rounded-full" alt="" />
+                                </button>
+
+                                <UserNavigationPanel />
+
+                            </div>
                         </>
                     ) : (
                         <>
