d309e54ca66abd722201fe541a05555a5067f988
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-09-15 12:12:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,29 +6,53 @@ import { lookInSession } from "./common/session";
 
 export const UserContext = createContext({})
 
-const App = () => {
+// const App = () => {
 
-    const [userAuth, setUserAuth] = useState();
+//     const [userAuth, setUserAuth] = useState();
+
+//     useEffect(() => {
+
+//         let userInSession = lookInSession("user");
+//         userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null })
+
+//     }, [])
+
+//     return (
+//         <UserContext.Provider value={{userAuth, setUserAuth}}>
+//         <Routes>
+//             <Route path="/" element={<Navbar/>}>
+//                 <Route path="/signin" element={<UserAuthForm type="sign-in"/>}/>
+//                 <Route path="/signup" element={<UserAuthForm type="sign-up"/>}/>
+//             </Route>
+
+//         </Routes>
+//         </UserContext.Provider>
+        
+//     )
+// }
+
+// export default App;
+
+
+
+const App = () => {
+    const [userAuth, setUserAuth] = useState({ access_token: null, profile_img: null });
 
     useEffect(() => {
-
         let userInSession = lookInSession("user");
-        userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null })
-
-    }, [])
+        userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null });
+    }, []);
 
     return (
-        <UserContext.Provider value={{userAuth, setUserAuth}}>
-        <Routes>
-            <Route path="/" element={<Navbar/>}>
-                <Route path="/signin" element={<UserAuthForm type="sign-in"/>}/>
-                <Route path="/signup" element={<UserAuthForm type="sign-up"/>}/>
-            </Route>
-
-        </Routes>
+        <UserContext.Provider value={{ userAuth, setUserAuth }}>
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+                    <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
+                </Route>
+            </Routes>
         </UserContext.Provider>
-        
-    )
-}
+    );
+};
 
 export default App;
